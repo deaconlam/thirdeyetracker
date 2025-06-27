ce4e7f43b5580c674bf9e65f71a972aec3b5ef8a
deaconlam <127363839+deaconlam@users.noreply.github.com>
2025-06-27 00:05:43 -0700

    Update

--- script.js
+++ script.js
@@ -1,28 +1,18 @@
 document.getElementById("login-form").addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const username = document.getElementById("username").value.trim();
   const password = document.getElementById("password").value;
 
   try {
-    const userDoc = await db.collection("usernames").doc(username).get();
-    if (!userDoc.exists) {
-      document.getElementById("error").innerHTML = "Couldn't find your account";
-      document.getElementById("password").style.border =  "1px solid #594413";
-      document.getElementById("username").style.border =  "1px solid rgb(140, 31, 31)";
-;
-
-      return;
-    }
-
-    const email = userDoc.data().email;
-    
-    await auth.signInWithEmailAndPassword(email, password);
+    await auth.signInWithEmailAndPassword("admin@myapp.local", password);
     window.location.href = "index.html";
-
   } catch (err) {
-    document.getElementById("error").innerHTML = "Wrong password";
-    document.getElementById("username").style.border =  "1px solid #594413";
+    if (err.code == "auth/too-many-requests") {
+      document.getElementById("error").innerHTML = "Too many requests";
+    }
+    else {
+      document.getElementById("error").innerHTML = "Wrong password";
+    }
     document.getElementById("password").style.border =  "1px solid rgb(140, 31, 31)";
   }
 });
